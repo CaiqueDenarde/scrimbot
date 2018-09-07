67203a8d1350ad3a29f07a45e161ef55581cc0e0
CaiqueDenarde <42156915+CaiqueDenarde@users.noreply.github.com>
2018-09-07 02:04:18 -0300

    Update scrim.py

--- scrim.py
+++ scrim.py
@@ -9,82 +9,53 @@ client = discord.Client()
 chave = chave.n_chave()
 
 
-
 @client.event
 async def on_ready():
     print('BOT ONLINE')
     print(client.user.name)
     print(client.user.id)
 
+
+
     @client.event
     async def on_message(text, ):
 
+        global time
+        global data
+        global hora
 
-            for cont1 in range (1):
+        for cont in range(1):
 
-                if text.content.lower().startswith('addscrim') :
-                    info = discord.Embed(
-                        title='Informe os dados abaixo!',
-                        description=
-                        '- Nome do Time\n'
-                        '. Data\n'
-                        ': Hora',
-                    )
-                    botmsg = await client.send_message(text.channel, embed=info)
-                    
-                    print('Iniciando Gravação...')
-                    #if text.author.id == '455265787226357760':  # Permissao
+            if text.content.lower().startswith('addscrim'):
+                info = discord.Embed(
+                    title='Informe os dados abaixo!',
+                    description='- Nome do Time\n'
+                                '. Data\n'
+                                ': Hora',
+                )
+                botmsg = await client.send_message(text.channel, embed=info)
+                print('Iniciando Gravação...')
 
-                    @client.event
-                    async def on_message(text, ):
-                        global time
-                        global data
-                        global hora
-                        global info
+            if text.content.lower().startswith('-'):
+                time = 'Nome do Time ' + text.content
+                print(time)
+                # await client.send_message(text.channel, time)
 
-                        if text.content.lower().startswith('-'):
-                                time = 'Nome do Time ' + text.content
-                                print(time)
-                                # await client.send_message(text.channel, time)
+            elif text.content.lower().startswith('.'):
+                data = 'Data ' + text.content
+                # await client.send_message(text.channel, data)
+                print(data)
+            elif text.content.lower().startswith(':'):
+                hora = 'Hora ' + text.content
+                # await client.send_message(text.channel, hora)
+                print(hora)
+            if text.content.lower().startswith("confscrim"):
+                info = discord.Embed(
+                    title='Scrim Confirmada!\n',
+                    description='{}\n {}\n {}\n'.format(time, data, hora)
 
-                        if text.content.lower().startswith('.'):
-                                data = 'Data ' + text.content
-                                # await client.send_message(text.channel, data)
-                                print(data)
-                        if text.content.lower().startswith(':'):
-                                hora = 'Hora ' + text.content
-                                # await client.send_message(text.channel, hora)
-                                print(hora)
-
-
-                        if text.content.lower().startswith('confscrim'):
-                            info = discord.Embed(
-                                title='Scrim Confirmada!\n',
-                                description='{}\n {}\n {}\n'.format(time, data, hora)
-
-                            )
-                            botmsg = await client.send_message(text.channel, embed=info)
-                            print(time, data, hora)
-
-
-            #else:
-                #await client.send_message(text.channel, 'Você não tem permissão!')
-
-            for cont2 in range(1):
-
-                    if text.content.lower().startswith('helpbot'):
-                        info = discord.Embed(
-                            title='Lista de Comandos',
-                            description=
-                            'Gravar dados (addscrim)\n'
-                            'Add Time ( - )\n'
-                            'Add Data ( . )\n'
-                            'Add Hora ( : )\n'
-                            'Confirmar dados (confscrim)\n'
-                            '\nUtilize os comandos sem parênteses'
-                        )
-                        botmsg = await client.send_message(text.channel, embed=info)
-                        # print(cont2)
+                )
+                botmsg = await client.send_message(text.channel, embed=info)
 
 
 client.run(chave)
